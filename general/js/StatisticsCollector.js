6ddd149765e9a46bd0d454ad1fa565fb224f11fa
jbizLT <pozolotusiko@mail.ru>
2022-09-24 01:29:16 +0300

    develop system: bot/human refresh all statistics in 15 seconds: save bot/human index and refresh.

--- general/js/StatisticsCollector.js
+++ general/js/StatisticsCollector.js
@@ -1,51 +1,61 @@
 class StatisticsCollector{
-    // button statistics
+    // button click statistics
     #minButtonClickTime = Number.MAX_SAFE_INTEGER; // time between two clicks
     #maxButtonClickTime = 0;
     #avgButtonClickTime = 0;
-    #sumOfTimeIntervals = 0;
+    #sumButtonClickTime = 0;
     #countClicks = 0;
+    // time&distance (index) between two clicks
+    #minIndexValue = Number.MAX_SAFE_INTEGER;
+    #maxIndexValue = 0;
+    #avgIndexValue = 0;
+    #sumIndexValue = 0;
+    #countIndexValue = 0;
     // moving on buttons statistics
     #minMouseOnButtonTime = Number.MAX_SAFE_INTEGER;
     #maxMouseOnButtonTime = 0;
     #avgMouseOnButtonTime = 0;
-    #sumOfMouseOnButtonTime = 0;
-    #countTimesOnButton = 0;
+    #sumMouseOnButtonTime = 0;
+    #countMouseOnButtonTime = 0;
     // keyboard statistics
     #minTapsPerSecond = Number.MAX_SAFE_INTEGER;
     #maxTapsPerSecond = 0;
     #avgTapsPerSecond = 0;
     #sumTapsPerSecond = 0;
     #countTapsOnKeys = 0;
-    #countTapIntervals = 0;
+    #countTapsIntervals = 0;
 
     #stats = document.querySelector('#stats');
 
     #updateAvgTimeClicks(timeInterval){
-        this.#sumOfTimeIntervals+=timeInterval;
-        this.#avgButtonClickTime = this.#sumOfTimeIntervals / (this.#countClicks - 1);
+        this.#sumButtonClickTime+=timeInterval;
+        this.#avgButtonClickTime = this.#sumButtonClickTime / (this.#countClicks - 1);
     }
     #updateAvgTimeOnButtons(timeInterval){
-        this.#sumOfMouseOnButtonTime+=timeInterval;
-        this.#avgMouseOnButtonTime = this.#sumOfMouseOnButtonTime / this.#countTimesOnButton;
+        this.#sumMouseOnButtonTime+=timeInterval;
+        this.#avgMouseOnButtonTime = this.#sumMouseOnButtonTime / this.#countMouseOnButtonTime;
     }
     #updateAvgTapsPerSecond(tapsPerSecond){
         this.#sumTapsPerSecond+=tapsPerSecond;
-        this.#avgTapsPerSecond = this.#sumTapsPerSecond / this.#countTapIntervals;
+        this.#avgTapsPerSecond = this.#sumTapsPerSecond / this.#countTapsIntervals;
+    }
+    #updateAvgIndexValue(index) {
+        this.#sumIndexValue+=index;
+        this.#avgIndexValue = this.#sumIndexValue / this.#countIndexValue;
     }
 
     incrementClicks() {
         this.#countClicks++;
     }
     incrementCountOnButtons(){
-        this.#countTimesOnButton++;
+        this.#countMouseOnButtonTime++;
     }
     incrementKeyTaps(){
         this.#countTapsOnKeys++;
         statistics.displayStatistics();
     }
     incrementTapIntervals(){
-        this.#countTapIntervals++;
+        this.#countTapsIntervals++;
     }
 
     takeTimeIntervalClicks(timeInterval){
@@ -79,22 +89,44 @@ class StatisticsCollector{
         }
         this.#updateAvgTapsPerSecond(tapsPerSecond);
     }
+    takeClicksCoordsTime(x_1, y_1, x_2, y_2, timeInterval) {
+        let distance;
+        let indexTimeDistance;
+        distance = Math.sqrt(Math.pow(Math.abs(x_1 - x_2),2) + Math.pow(Math.abs(y_1 - y_2),2));
+        console.log(timeInterval + ' ' + distance);
+        if (distance === 0) {
+            distance = 1;
+        }
+        indexTimeDistance = timeInterval / distance;
+        this.#countIndexValue++;
+        if (indexTimeDistance > this.#maxIndexValue) {
+            this.#maxIndexValue = indexTimeDistance;
+        }
+        if (indexTimeDistance < this.#minIndexValue) {
+            this.#minIndexValue = indexTimeDistance;
+        }
+        this.#updateAvgIndexValue(indexTimeDistance);
+    }
 
     getCountTapsOnKeys() {
         return this.#countTapsOnKeys;
     }
 
     displayStatistics(){
-        this.#stats.innerText = 'Click statistics:\n' +
+        this.#stats.innerText = 'Click button statistics:\n' +
             'Average time between two clicks: ' + this.#avgButtonClickTime.toFixed(2) + '\n' +
             'Max. time between two clicks: ' + this.#maxButtonClickTime + '\n' +
             'Min. time between two clicks: ' + this.#minButtonClickTime + '\n' +
             'Total amount of clicks: ' + this.#countClicks + '\n' +
+            '\nClick index (time/distance) statistics:\n' +
+            'Average index value: ' + this.#avgIndexValue.toFixed(5) + '\n' +
+            'Max. index value: ' + this.#maxIndexValue.toFixed(5) + '\n' +
+            'Min. index value: ' + this.#minIndexValue.toFixed(5) + '\n' +
             '\nOn-button statistics:\n' +
             'Average time on buttons: ' + this.#avgMouseOnButtonTime.toFixed(2) + '\n' +
             'Max. time on buttons: ' + this.#maxMouseOnButtonTime + '\n' +
             'Min. time on buttons: ' + this.#minMouseOnButtonTime + '\n' +
-            'Total amount of being on buttons: ' + this.#countTimesOnButton + '\n' +
+            'Total amount of being on buttons: ' + this.#countMouseOnButtonTime + '\n' +
             '\nKeyboard statistics:\n' +
             'Average taps per second: ' + this.#avgTapsPerSecond.toFixed(2) + '\n' +
             'Max. taps per second: ' + this.#maxTapsPerSecond + '\n' +
--- general/js/app.js
+++ general/js/app.js
@@ -1,19 +1,28 @@
 'use strict';
 // timers visualization
-const milliElementClick = document.querySelector('.milliseconds');
-const milliElementOnButton = document.querySelector('.milliseconds1');
-const milliElementKeyboard = document.querySelector('.milliseconds2');
+const milliElementClick = document.querySelector('.msClick');
+const milliElementOnButton = document.querySelector('.msOnButton');
+const milliElementKeyboard = document.querySelector('.msKeyboard');
+const milliElementClickDistance = document.querySelector('.msClickDistance');
+// html block - wrapper (with buttons)
+const wrapper = document.querySelector('.wrapper');
 // time counters & intervals for timers for each event
-let milliSecondsClick = 0, intervalClick;
-let milliSecondsOnButton = 0, intervalOnButton;
-let milliSecondsOnKeyboard = 0, intervalKeyboard;
+let msClick = 0, intervalClick;
+let msClickDistance = 0, intervalClickDistance;
+let msOnButton = 0, intervalOnButton;
+let msOnKeyboard = 0, intervalKeyboard;
+// coordinates of clicks
+let firstClickX = 0;
+let firstClickY = 0;
+let secondClickX = 0;
+let secondClickY = 0;
 // timer start/stop flags
 let timerStartStopClick = false;
+let timerStartStopClickDistance = true;
 let timerStartStopOnButton = true;
 let timerStartStopKeyboard = true;
 // timers intervals
 let intervalTimerStopMillisClick = 0;
-/*let timerStopMillisKeyboard = 0;*/
 // keyboard's start
 let keyboardStartTaps = 0;
 let keyboardStopTaps = 0;
@@ -22,43 +31,50 @@ const statistics = new StatisticsCollector();
 
 // timers starters
 function startTimerClick(){
-    milliSecondsClick++;
-    if(milliSecondsClick < 9){
-        milliElementClick.innerText = "0" + milliSecondsClick;
+    msClick++;
+    if(msClick < 9){
+        milliElementClick.innerText = '0' + msClick;
     } else {
-        milliElementClick.innerText = milliSecondsClick;
+        milliElementClick.innerText = msClick;
     }
-    if (milliSecondsClick >= 500) {
+    if (msClick >= 500) {
         stopTimerClick(intervalClick);
     }
 }
 function startTimerOnButton(){
-    milliSecondsOnButton++;
-    if(milliSecondsOnButton < 9){
-        milliElementOnButton.innerText = "0" + milliSecondsOnButton;
+    msOnButton++;
+    if(msOnButton < 9){
+        milliElementOnButton.innerText = '0' + msOnButton;
     } else {
-        milliElementOnButton.innerText = milliSecondsOnButton;
+        milliElementOnButton.innerText = msOnButton;
     }
-    if (milliSecondsOnButton >= 500) {
+    if (msOnButton >= 500) {
         stopTimerOnButton(intervalOnButton);
     }
 }
-function startTimerKeyboard(){
-    milliSecondsOnKeyboard++;
-    if(milliSecondsOnKeyboard < 9){
-        milliElementKeyboard.innerText = "0" + milliSecondsOnKeyboard;
+function startTimerKeyboard() {
+    msOnKeyboard++;
+    if (msOnKeyboard < 9) {
+        milliElementKeyboard.innerText = '0' + msOnKeyboard;
     } else {
-        milliElementKeyboard.innerText = milliSecondsOnKeyboard;
+        milliElementKeyboard.innerText = msOnKeyboard;
     }
-    if (milliSecondsOnKeyboard % 100 === 0){
+    if (msOnKeyboard % 100 === 0) {
         keyboardStopTaps = statistics.getCountTapsOnKeys();
         statistics.incrementTapIntervals();
-        console.log(keyboardStartTaps + ' ' + keyboardStopTaps)
         statistics.takeTapsPerSecondKeyboard(keyboardStopTaps - keyboardStartTaps);
         keyboardStartTaps = keyboardStopTaps;
     }
-    if (milliSecondsOnKeyboard > 100) {
+    if (msOnKeyboard > 100) {
         stopTimerKeyboard(intervalKeyboard);
+    }
+}
+function startTimerClickDistance() {
+    msClickDistance++;
+    if (msClickDistance < 9) {
+        milliElementClickDistance.innerText = '0' + msClickDistance;
+    } else {
+        milliElementClickDistance.innerText = msClickDistance;
     }
 }
 
@@ -75,13 +91,10 @@ function stopTimerOnButton(interval){
 function stopTimerKeyboard(interval){
     clearInterval(interval);
     timerStartStopKeyboard = true;
-    milliSecondsOnKeyboard = 0;
+    msOnKeyboard = 0;
     milliElementKeyboard.innerText = '00';
     statistics.displayStatistics();
 }
-
-// html block - wrapper (with buttons)
-const wrapper = document.querySelector('.wrapper');
 
 // handler's functions
 function handleMouseEnter(){
@@ -93,21 +106,21 @@ function handleMouseEnter(){
 function handleMouseLeave(){
     timerStartStopOnButton = true;
     statistics.incrementCountOnButtons();
-    statistics.takeTimeIntervalsOnButtons(milliSecondsOnButton);
-    milliSecondsOnButton = 0;
+    statistics.takeTimeIntervalsOnButtons(msOnButton);
+    msOnButton = 0;
     stopTimerOnButton(intervalOnButton);
 }
 function handleMouseClick(){
     timerStartStopClick = !timerStartStopClick;
     statistics.incrementClicks();
     if (timerStartStopClick) {
-        milliSecondsClick = 0;
+        msClick = 0;
         intervalClick = setInterval(startTimerClick, 10);
     } else {
         stopTimerClick(intervalClick);
-        intervalTimerStopMillisClick = milliSecondsClick;
+        intervalTimerStopMillisClick = msClick;
         statistics.takeTimeIntervalClicks(intervalTimerStopMillisClick);
-        milliSecondsClick = 0;
+        msClick = 0;
         intervalClick = setInterval(startTimerClick, 10);
         timerStartStopClick = !timerStartStopClick;
     }
@@ -116,7 +129,7 @@ function handleMouseClick(){
 // keyboard taps handler
 document.onkeydown = function (){
     if (timerStartStopKeyboard) {
-        milliSecondsOnKeyboard = 0;
+        msOnKeyboard = 0;
         keyboardStartTaps = statistics.getCountTapsOnKeys();
         intervalKeyboard = setInterval(startTimerKeyboard, 10);
     }
@@ -124,7 +137,28 @@ document.onkeydown = function (){
     timerStartStopKeyboard = false;
 }
 
+// click coordinates handler
+document.addEventListener('click', (event) => {
+    if (timerStartStopClickDistance) {
+        firstClickX = event.pageX;
+        firstClickY = event.pageY;
+        intervalClickDistance = setInterval(startTimerClickDistance, 100);
+        timerStartStopClickDistance = false;
+    } else {
+        secondClickX = event.pageX;
+        secondClickY = event.pageY;
+        statistics.takeClicksCoordsTime(firstClickX, firstClickY, secondClickX, secondClickY, msClickDistance);
+        statistics.displayStatistics();
+        firstClickX = secondClickX;
+        firstClickY = secondClickY;
+        msClickDistance = 0;
+        milliElementClickDistance.innerText = '00';
+    }
+    console.log(event.pageX + ' ' + event.pageY);
+})
+
 // link functions with events
 wrapper.addEventListener('click', handleMouseClick);
 wrapper.addEventListener('mouseenter', handleMouseEnter);
 wrapper.addEventListener('mouseleave', handleMouseLeave);
+
